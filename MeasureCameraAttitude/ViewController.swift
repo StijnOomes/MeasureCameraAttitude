8b6062bc48b9e657a631edb89a16a4461d6573a8
Stijn Oomes <stijn.oomes@gmail.com>
2016-11-02 16:33:13 +0100

    Initial Commit

---
+++ MeasureCameraAttitude/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MeasureCameraAttitude
+//
+//  Created by Stijn Oomes on 02/11/2016.
+//  Copyright © 2016 Oomes Vision Systems. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
